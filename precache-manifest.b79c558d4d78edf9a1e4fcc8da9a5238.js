030e072af96c29e2ae3214f1f15ab7bcb84201e5
Huaiqing <wenhuaiqing@hotmail.com>
2018-10-19 23:39:12 +1000

    Updates

---
+++ precache-manifest.b79c558d4d78edf9a1e4fcc8da9a5238.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaba6af65f2304e90670",
+    "url": "/reactclientpanel/static/js/runtime~main.eaba6af6.js"
+  },
+  {
+    "revision": "e1c4097cbaec6fdd31b4",
+    "url": "/reactclientpanel/static/js/main.e1c4097c.chunk.js"
+  },
+  {
+    "revision": "b5207778dc6b0cc23463",
+    "url": "/reactclientpanel/static/js/1.b5207778.chunk.js"
+  },
+  {
+    "revision": "e1c4097cbaec6fdd31b4",
+    "url": "/reactclientpanel/static/css/main.76ae44b1.chunk.css"
+  },
+  {
+    "revision": "2c731ae3a29adc97e506abb038a88861",
+    "url": "/reactclientpanel/index.html"
+  }
+];
