309606c672355ab0858f1dba2d2c22f3e1b89e9e
Huaiqing <wenhuaiqing@hotmail.com>
2018-10-19 23:35:21 +1000

    first commit

---
+++ build/precache-manifest.2383a881554734ceffa152d0f1404c7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a246749303433c28fa10",
+    "url": "/static/js/main.a2467493.chunk.js"
+  },
+  {
+    "revision": "7ca6a605e3a0b955e45f",
+    "url": "/static/js/1.7ca6a605.chunk.js"
+  },
+  {
+    "revision": "a246749303433c28fa10",
+    "url": "/static/css/main.76ae44b1.chunk.css"
+  },
+  {
+    "revision": "5ad8ced8c312de1aa8fa4d9f90c0ab5f",
+    "url": "/index.html"
+  }
+];
